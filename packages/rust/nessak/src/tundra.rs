8d38e9c535beb5b5f8fde77ff8d99e7455e58a9f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-08-27 18:18:00 +0200

    feat: added the tundra impl trait

--- packages/rust/nessak/src/tundra.rs
+++ packages/rust/nessak/src/tundra.rs
@@ -1,1 +1,21 @@
+pub trait TundraImplementation {
+    fn expand_generate(&self, n: usize, o: &[u8], w: usize) -> u8;
 
+    fn permutation_inner(
+        &self,
+        state: &mut [u32],
+        harmonizer: &[u32],
+        inner_permutation_rounds: usize,
+    );
+
+    fn compress_inner(&self, part_a: &mut [u32], part_b: &[u32], round: usize);
+
+    fn descent_compression_inner(
+        &self,
+        lane_a: &mut [u32],
+        part_b: &[u32],
+        k: usize,
+        p: usize,
+        r: usize,
+    ) -> Vec<u32>;
+}
